2eabaece88adf5b1d9c2e699f3077df96b57975b
david8868 <david.yu@uicentric.com>
2018-07-13 17:38:20 -0400

    submit what I have

--- app/src/main/java/com/ztony/davidyu/rxjavademo/Generic.java
+++ app/src/main/java/com/ztony/davidyu/rxjavademo/Generic.java
@@ -6,7 +6,7 @@ package com.ztony.davidyu.rxjavademo;
 
 //此处T可以随便写为任意标识，常见的如T、E、K、V等形式的参数常用于表示泛型
 //在实例化泛型类时，必须指定T的具体类型
-public class Generic<T>{
+public class Generic<T extends Number>{
     //key这个成员变量的类型为T,T的类型由外部指定
     private T key;
 
@@ -19,17 +19,4 @@ public class Generic<T>{
     }
 
 
-
-    public <T>T showKeyName(Generic<T> container)
-    {
-
-        T test = container.getKey();
-        return test;
-
-    }
-
-    public void showKey(T genericaObject)
-    {
-
-    }
 }
--- app/src/main/java/com/ztony/davidyu/rxjavademo/MainActivity.java
+++ app/src/main/java/com/ztony/davidyu/rxjavademo/MainActivity.java
@@ -656,14 +656,14 @@ public class MainActivity extends AppCompatActivity {
         //printMsg("111",333,"32423.4",44.44);
 
 
-        Generic<String> generic1 = new Generic<String>("11111");
+        //Generic<String> generic1 = new Generic<String>("11111");
         Generic<Integer> generic2 = new Generic<Integer>(2222);
         Generic<Float> generic3 = new Generic<Float>(2.4f);
         Generic<Double> generic4 = new Generic<Double>(2.56);
 
 
         //这一行代码编译器会提示错误，因为String类型并不是Number类型的子类
-      showKeyValue1(generic1);
+      // showKeyValue1(generic1);
 
         showKeyValue1(generic2);
         showKeyValue1(generic3);
