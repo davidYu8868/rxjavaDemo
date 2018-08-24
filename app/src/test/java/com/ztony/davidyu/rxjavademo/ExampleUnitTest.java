fe629310bbb86ea3fe293191ff262edf09bb559a
david8868 <david.yu@uicentric.com>
2018-08-24 16:51:07 -0400

    up to 539

--- app/src/test/java/com/ztony/davidyu/rxjavademo/ExampleUnitTest.java
+++ app/src/test/java/com/ztony/davidyu/rxjavademo/ExampleUnitTest.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.TestScheduler;
 
 import static org.junit.Assert.*;
 
@@ -157,6 +159,97 @@ public class ExampleUnitTest {
     }
 
     //536
+
+
+    @Test
+    public void usingTestObserver()
+    {
+
+        //An Observable with 5 one-second emissions
+        Observable<Long> source = Observable.interval(1, TimeUnit.SECONDS) .take(5);
+
+
+        //Declare TestObserver
+        TestObserver<Long> testObserver = new TestObserver<>();
+
+
+        //Assert no subscription has occurred yet
+        testObserver.assertNotSubscribed();
+
+     //Subscribe TestObserver to source
+        source.subscribe(testObserver);
+
+     //Assert TestObserver is subscribed
+        testObserver.assertSubscribed();
+
+
+        //Block and wait for Observable to terminate
+        testObserver.awaitTerminalEvent();
+
+
+        //Assert TestObserver called onComplete()
+        testObserver.assertComplete();
+
+
+       //Assert there were no errors
+        testObserver.assertNoErrors();
+
+
+        //Assert 5 values were received
+        testObserver.assertValueCount(5);
+
+
+        //Assert the received emissions were 0, 1, 2, 3, 4
+
+        testObserver.assertValues(0L, 1L, 2L, 3L, 4L);
+
+
+
+
+
+    }
+
+
+    @Test
+    public void usingTestScheduler()
+    {
+
+        //Declare TestScheduler
+        TestScheduler testScheduler = new TestScheduler();
+
+
+        //Declare TestObserver
+        TestObserver<Long> testObserver = new TestObserver<>();
+
+
+        Observable<Long> minuteTicker = Observable.interval(1, TimeUnit.MINUTES, testScheduler);
+
+        //Subscribe to TestObserver
+        minuteTicker.subscribe(testObserver);
+
+
+        //fast forward by 30 seconds
+        testScheduler.advanceTimeBy(30,TimeUnit.SECONDS);
+
+
+        //Assert no emissions have occurred yet
+        testObserver.assertValueCount(0);
+
+        //Fast forward to 70 seconds after subscription
+        testScheduler.advanceTimeTo(70, TimeUnit.SECONDS);
+
+        //Assert the first emission has occurred
+        testObserver.assertValueCount(1);
+
+
+        //Fast Forward to 90 minutes after subscription
+        testScheduler.advanceTimeTo(90, TimeUnit.MINUTES);
+
+
+        //Assert 90 emissions have occurred
+        testObserver.assertValueCount(90);
+
+    }
 
 
 }
